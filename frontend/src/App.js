dfafaf3fb2e8ed2216b9f592d2445ea690e31c56
Saad Mohamed Al Amine <mohamed.alamin.saad@ensia.edu.dz>
2025-11-19 22:29:28 +0100

    implement the prescription management page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Payments from './pages/Payments';
 import PatientList from './pages/PatientList';
+import PrescriptionManagement from './pages/MedicamentsDrugList'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/payments" element={<Payments />} />
         <Route path="/patient-list" element={<PatientList />} />
+        <Route path="/prescription-management" element={<PrescriptionManagement />} />
       </Routes>
     </Router>
   );
