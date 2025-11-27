58e3132d7a8bcd43fff025a69a475ee88c747eb3
Danil Ouakli <ouaklidanil@gmail.com>
2025-11-27 14:58:22 +0100

    Added backend folder and structure for our project

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-
-// It acts as a bridge between Electron (backend) and your React UI (frontend). You can skip it if you don’t need backend-to-frontend communication.
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  message: () => console.log('Hello from Electron preload!'),
-});
